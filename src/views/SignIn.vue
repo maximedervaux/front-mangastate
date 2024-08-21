26ea427184dc955d77c8875ba8b387e5e3331959
mdervaux <maxime.dervaux@ittalent.fr>
2024-08-21 14:32:40 +0200

    signin form

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container">
     <form class="form" @submit.prevent="login">
-      <img class="logoAstate" src='../assets/icon-manga.webp'>
+      <div style="text-align: center;">
+        <img class="logoAstate" src='../assets/icon-manga.webp'>
+        <h2>Welcome Back</h2>
+        <p>Vous n'avez pas encore de compte ? <router-link to="/signup">Créez le maintenant!</router-link></p>
+      </div>
       <div class="input">
         <label for="username">Username</label>
         <InputText id="username" v-model="username" />
@@ -12,12 +16,17 @@
         <InputText id="password" v-model="password" type="password" />
       </div>
 
+      <div class="forgot-password">
+        <router-link to="/forgot-password">Mot de passe oublié ?</router-link>
+      </div>
+
       <Button type="submit" label="Connexion" class="btnSignIn" icon="pi pi-user"/>
       <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
       <p v-if="token" class="token">Token: {{ token }}</p>
     </form>
   </div>
 </template>
+
 
 <script setup>
 import { ref } from 'vue';
@@ -48,43 +57,44 @@ const login = async () => {
 .btnSignIn{
   width: 95%;
   margin:15px 0px ;
+  color: var(--p-overlay-modal-color);
 }
 .logoAstate{
   max-width: 126px;
 }
 .container{
- display: flex;
- align-items: center;
- justify-content:center ;
- height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
 }
 .form {
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 30%;
-  border-radius: 2%;
-  background-color: #18181b;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;
+  border-radius: var(--p-content-border-radius);
+  background-color: var(--p-overlay-modal-background); 
+  box-shadow: var(--shadow-2); 
+  color: var(--p-overlay-modal-color);
 }
 .input {
   display: flex;
   flex-direction: column;
   width: 95%;
 }
-.passwd {
-  width: 100%;
-}
-.inputStyle {
-  width: 100%;
-}
 .error {
-  color: red;
+  color: var(--red-500); 
   margin-top: 10px;
 }
 .token {
-  color: green;
+  color: var(--green-500); 
   margin-top: 10px;
   word-break: break-all;
+}
+
+.forgot-password{
+  text-align: right;
+  width: 90%;
 }
 </style>
