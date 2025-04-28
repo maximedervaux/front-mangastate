72c31a7416085462152f2cd9af8ee53a42513173
maximedervaux <maximeder.pro@gmail.com>
2025-04-28 21:31:16 +0200

    Ajout de la gestion des cookies pour toutes les requêtes API

--- src/common/api.service.js
+++ src/common/api.service.js
@@ -2,53 +2,56 @@ import axios from 'axios';
 import { API_URL } from './config';
 
 const ApiService = {
-  // Méthode générique pour les requêtes GET
   async get(resource, slug = '') {
     try {
-      
-      const response = await axios.get(`${API_URL}/${resource}/${slug}`);
-      // console.log(response);
+      const response = await axios.get(`${API_URL}/${resource}/${slug}`, {
+        withCredentials: true,  
+      });
       return response.data;
     } catch (error) {
       throw new Error(`API Error occurred: ${error.response}`);
     }
   },
 
-  // Méthode générique pour les requêtes POST
   async post(resource, params) {
     try {
-      const response = await axios.post(`${API_URL}/${resource}`, params);
+      const response = await axios.post(`${API_URL}/${resource}`, params, {
+        withCredentials: true, 
+      });
       return response.data;
     } catch (error) {
       throw new Error(`API Error occurred: ${error.response}`);
     }
   },
 
-  // Méthode générique pour les requêtes PUT
   async update(resource, slug, params) {
     try {
-      const response = await axios.put(`${API_URL}/${resource}/${slug}`, params);
+      const response = await axios.put(`${API_URL}/${resource}/${slug}`, params, {
+        withCredentials: true,  
+      });
       return response.data;
     } catch (error) {
       throw new Error(`API Error occurred: ${error.response}`);
     }
   },
 
-  // Méthode générique pour les requêtes DELETE
   async delete(resource, slug) {
     try {
-      const response = await axios.delete(`${API_URL}/${resource}/${slug}`);
+      const response = await axios.delete(`${API_URL}/${resource}/${slug}`, {
+        withCredentials: true,  
+      });
       return response.data;
     } catch (error) {
       throw new Error(`API Error occurred: ${error.response}`);
     }
   },
 
-  // Méthode pour les requêtes de type query avec paramètres
   async query(resource, params) {
     try {
-      const response = await axios.get(`${API_URL}/${resource}`, { params: params });
-      // console.log(response);
+      const response = await axios.get(`${API_URL}/${resource}`, {
+        params: params,
+        withCredentials: true,  
+      });
       return response.data;
     } catch (error) {
       throw new Error(`API Error occurred: ${error.response}`);
